d36a8ac1bb7510b3eb946d0f855421beb5673c6a
Matt Schmulen <matt.schmulen@gmail.com>
2020-04-14 08:28:21 -0500

    config for in memory sqlite

--- Sources/App/Controllers/TodoController.swift
+++ Sources/App/Controllers/TodoController.swift
@@ -2,15 +2,16 @@ import Fluent
 import Vapor
 
 struct TodoController {
+    
     func index(req: Request) throws -> EventLoopFuture<[Todo]> {
         return Todo.query(on: req.db).all()
     }
 
     func create(req: Request) throws -> EventLoopFuture<Todo> {
-        let todo = try req.content.decode(Todo.self)
-        return todo.save(on: req.db).map { todo }
+        let model = try req.content.decode(Todo.self)
+        return model.save(on: req.db).map { model }
     }
-
+    
     func delete(req: Request) throws -> EventLoopFuture<HTTPStatus> {
         return Todo.find(req.parameters.get("todoID"), on: req.db)
             .unwrap(or: Abort(.notFound))
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,10 +8,12 @@ public func configure(_ app: Application) throws {
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     // Configure SQLite database
-    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    app.databases.use(.sqlite(.memory), as: .sqlite)
 
     // Configure migrations
     app.migrations.add(CreateTodo())
+    
+    try app.autoMigrate().wait()
     
     try routes(app)
 }
