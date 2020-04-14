049f2296220d04fd569cb5be908d55286ed941fe
Matt Schmulen <matt.schmulen@gmail.com>
2020-04-14 09:02:07 -0500

    add property

--- Sources/App/Models/Todo.swift
+++ Sources/App/Models/Todo.swift
@@ -9,11 +9,15 @@ final class Todo: Model, Content {
 
     @Field(key: "title")
     var title: String
-
+    
+    @Field(key: "name")
+    var name: String
+    
     init() { }
 
-    init(id: UUID? = nil, title: String) {
+    init(id: UUID? = nil, title: String, name: String) {
         self.id = id
         self.title = title
+        self.name = name
     }
 }
